400c9ee6646275a7a32d3f1527b1e14f93d9f83f
tarrsalah <tarrsalah@gmail.com>
2014-10-02 16:30:48 +0100

    Publish /api/tasks resource.

--- main.go
+++ main.go
@@ -1,27 +1,82 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gohttp/app"
+	"github.com/gohttp/logger"
 	"github.com/gohttp/response"
-	"github.com/tarrsalah/DooDooDooDoo/db"
+	"github.com/tarrsalah/gohttp-todo-example/db"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	db.BootStrap()
-	app := app.New()
-	app.Get("/api/tasks", getAllTasks)
-	// app.Get("/api/tasks/:id", getTask)
-	// app.Post("/api/tasks", submitNewTask)
-	// app.De("/api/tasks/:id", deleteTask)
-	app.Listen(":3000")
-}
+	a := app.New()
+	a.Use(logger.New())
 
-func getAllTasks(w http.ResponseWriter, r *http.Request) {
-	var tasks []*db.Task
-	err := db.Map.Select(&tasks, "select * from task")
-	if err != nil {
-		panic(err)
-	}
-	response.OK(w, tasks)
+	// serve static files
+	fs := http.FileServer(http.Dir("./static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	a.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/index.html")
+	})
+
+	a.Get("/api/tasks", func(w http.ResponseWriter, r *http.Request) {
+		var tasks []*db.Task
+		err := db.Map.Select(&tasks, "select * from task")
+		if err != nil {
+			response.InternalServerError(w)
+			return
+		}
+		response.OK(w, tasks)
+	})
+
+	a.Get("/api/tasks/:id", func(w http.ResponseWriter, r *http.Request) {
+		task := db.NewTask("")
+		err := db.Map.Get(task, r.URL.Query().Get(":id"))
+		if err != nil {
+			response.NotFound(w)
+			return
+		}
+		response.OK(w, task)
+	})
+
+	a.Post("/api/tasks", func(w http.ResponseWriter, r *http.Request) {
+		task := db.NewTask("")
+		dec := json.NewDecoder(r.Body)
+		err := dec.Decode(task)
+		if err != nil {
+			response.InternalServerError(w)
+			return
+		}
+		err = db.Map.Insert(task)
+		if err != nil {
+			response.InternalServerError(w)
+			return
+		}
+		response.Created(w)
+	})
+
+	a.Del("/api/tasks/:id", func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		task := db.NewTask("")
+		task.ID, err = strconv.Atoi(r.URL.Query().Get(":id"))
+		if err != nil {
+			fmt.Println(err)
+			response.InternalServerError(w)
+			return
+		}
+		_, err = db.Map.Delete(task)
+		if err != nil {
+			fmt.Println(err)
+			response.InternalServerError(w)
+			return
+		}
+		response.NoContent(w)
+
+	})
+	a.Listen(":3000")
 }
